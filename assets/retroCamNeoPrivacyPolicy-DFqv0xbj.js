2c62c04cd076c446a43c1ac83a840636d67db18b
Onur Ustunel <ustunel.onur@gmail.com>
2026-04-25 12:31:42 +0200

    Deploy RetroCamNeo legal page updates

---
+++ assets/retroCamNeoPrivacyPolicy-DFqv0xbj.js
@@ -0,0 +1,1 @@
+import{c as o,j as e}from"./index-CzrUhFfi.js";import{R as t}from"./retrocamneo-privacy-policy-BlTa7G2Y.js";o.createRoot(document.getElementById("root")).render(e.jsx(t,{}));
---
+++ assets/retroCamNeoTermsOfUse-D5Ebu9fi.js
@@ -0,0 +1,1 @@
+import{c as e,j as o}from"./index-CzrUhFfi.js";import{R as t}from"./retrocamneo-terms-of-use-CAEp9p0q.js";e.createRoot(document.getElementById("root")).render(o.jsx(t,{}));
